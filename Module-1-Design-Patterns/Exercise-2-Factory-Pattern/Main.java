b0766bee24b22ddcbb48f2fb3b551cae18c6c041
Shubhambehera29 <shubhambehera29@gmail.com>
2026-06-25 11:41:47 +0530

    Addeed Exercise 2

---
+++ Module-1-Design-Patterns/Exercise-2-Factory-Pattern/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        DocumentFactory factory = new PdfFactory();
+
+        Document document = factory.createDocument();
+
+        document.open();
+
+    }
+
+}
